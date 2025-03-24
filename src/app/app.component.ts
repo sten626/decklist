5f8a13c01465a915e273969095e73310eab5db01
Sten Indzeoski <steven.indzeoski@gmail.com>
2025-03-23 22:29:57 -0600

    Adding angular material and starting on page layout.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { provideNativeDateAdapter } from "@angular/material/core";
+// import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  selector: "app-root",
+  // imports: [RouterOutlet],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss",
+  imports: [MatDatepickerModule, MatFormFieldModule, MatInputModule],
+  providers: [provideNativeDateAdapter()],
 })
 export class AppComponent {
-  title = 'decklist';
+  title = "decklist";
 }
